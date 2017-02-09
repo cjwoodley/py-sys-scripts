75d03748f16a9d063868f981b48893552dbbbcfa
C.J. Woodley <cecil.woodley@gmail.com>
2017-02-08 21:41:14 -0600

    Update cliPIPInstall.py

--- cliPIPInstall.py
+++ cliPIPInstall.py
@@ -1,1 +1,5 @@
 sudo -H pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip install -U
+
+#pip list --outdated --format=freeze
+
+#http://stackoverflow.com/questions/2720014/upgrading-all-packages-with-pip
